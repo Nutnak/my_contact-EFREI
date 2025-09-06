567db08a9f4a3024e95503fd4ff7440a777224ad
Quentin <quentinucme@hotmail.fr>
2025-09-06 22:00:20 +0200

    fix: fix the route issue, time to work on insert data in DB

--- server/schemas/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
-
-const userSchema = new Schema({
-    email: String,
-    password: String,
-}, { timestamps: true });
-
-export default userSchema;
