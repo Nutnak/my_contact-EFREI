d588550b19b245b278bb03b0c14ace95dd59dad2
Quentin <quentinucme@hotmail.fr>
2025-09-10 18:52:57 +0200

    feature: login and register done

--- server/route/router.js
+++ server/route/router.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { createUser } from '../controller/auth/register.js'
+import { loginUser } from '../controller/auth/login.js';
 const route = express.Router();
 
 
 // Auth routes
 route.post('/auth/register', createUser)
+route.post('/auth/login', loginUser)
 // User routes
 // route.post('/user', createUser);
 
