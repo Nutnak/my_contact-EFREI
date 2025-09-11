d72829852cd57e2fdcdd6a17fffa62b0ecba6409
Quentin <quentinucme@hotmail.fr>
2025-09-11 09:11:32 +0200

    fix: user model

---
+++ server/controller/contact/addContact.js
@@ -0,0 +1,5 @@
+import { User } from '../../model/user.js'
+
+export const addContact = (req, res) => {
+    const user = req.header
+}
--- server/route/router.js
+++ server/route/router.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { createUser } from '../controller/auth/register.js'
 import { loginUser } from '../controller/auth/login.js';
-import { authMiddleware } from '../controller/middleware/authMiddleware.js'
+import { authMiddleware } from '../middleware/authMiddleware.js'
+import { test } from '../middleware/testMiddleware.js'
 const route = express.Router();
 
 
@@ -9,8 +10,6 @@ const route = express.Router();
 route.post('/auth/register', createUser)
 route.post('/auth/login', loginUser)
 // User routes
-route.get('/test', authMiddleware, (req, res) => {
-    res.json({message: 'Route protégée', user: req.user})
-})
+route.get('/test', test);
 
 export default route;
