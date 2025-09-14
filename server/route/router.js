393ac3fc3f7959243d27ec6b9e218cf6a8298dfd
Quentin <quentinucme@hotmail.fr>
2025-09-14 12:03:17 +0200

    :feature: just add the delete logic

--- server/route/router.js
+++ server/route/router.js
@@ -4,6 +4,7 @@ import { loginUser } from '../controller/auth/login.js';
 import { authMiddleware } from '../middleware/authMiddleware.js'
 import { addContact } from '../controller/contact/addContact.js'
 import { updateContact } from '../controller/contact/updateContact.js';
+import { deleteContact } from '../controller/contact/deleteContact.js';
 // import { test } from '../middleware/testMiddleware.js'
 const route = express.Router();
 
@@ -11,8 +12,9 @@ const route = express.Router();
 // Auth routes
 route.post('/auth/register', createUser)
 route.post('/auth/login', loginUser)
-// User routes
+// Contact routes
 route.post('/contact/add/', authMiddleware, addContact);
 route.post('/contact/update/:id', authMiddleware, updateContact)
+route.post('/contact/delete/:id', authMiddleware, deleteContact)
 
 export default route;
