f7bc2e7f9174d5c038e46f87cdcde1ebb81056a8
Quentin <quentinucme@hotmail.fr>
2025-09-29 23:22:42 +0200

    :feature: back, add the token with cookie

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export const Dashboard = () => {
-    return(
-        <h1>Dashboard page</h1>
-    )
-        
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/dashboard', { withCredentials: true })
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                if (error.response && error.response.status === 403) {
+                    navigate('/login'); // Redirige si token absent ou invalide
+                    console.log(error.response)
+                }
+            });
+    }, [navigate]);
+
+    return (
+        <>
+            <h1>Dashboard page</h1>
+            {/* Ton contenu */}
+        </>
+    );
 };
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,20 +2,21 @@ import jwt from 'jsonwebtoken'
 
 export const authMiddleware = (req, res, next) => {
     const token = req.cookies.accesToken;
-    if(!token){
-        return res.status(403).json({message: "Aucun token renseigné."})
+
+    if (!token && token === undefined) {
+        return res.status(403).json({ message: "Aucun token renseigné." })
     }
 
     try {
         const tokenWithoutBearer = token.split(' ')[1];
-        
+
         const decoded = jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET_KEY)
-    
+
         // console.log(decoded.userid)
         req.userId = decoded.userid
         next()
     } catch (err) {
-        res.status(401).json({message: "Token invalidé"})
+        res.status(401).json({ message: "Token invalidé" })
     }
-    
+
 };
